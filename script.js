9fb1328c8bcfbf055f44e9f426da2f84ed662afa
annntthhh <anethblanco2010@gmail.com>
2026-01-30 20:21:09 +0100

    Add function to show surprise message

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarSorpresa() {
+    const texto = document.getElementById("texto-sorpresa");
+    texto.classList.toggle("oculto");
+    alert("❤️ ¡Espero que tengas un día increíble! ❤️");
+}
+
+// Puedes añadir aquí un contador de días si sabes la fecha en que empezaron
